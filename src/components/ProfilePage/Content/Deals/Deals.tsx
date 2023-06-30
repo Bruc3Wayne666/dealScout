a658995bf21728908abacf0545c7e03fa3a95cf2
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2023-06-30 20:04:36 +0300

    first commit

--- src/components/ProfilePage/Content/Deals/Deals.tsx
+++ src/components/ProfilePage/Content/Deals/Deals.tsx
@@ -6,6 +6,30 @@ import {Plan, Time} from "../../../../api/deal";
 import DealCard from "./DealCard/DealCard";
 import {ThemeContext, ThemeContextType} from "../../../../providers/ThemeProvider";
 import {deal} from "../../../../temp";
+import {capitalize} from "../../../../shared/utils";
+
+
+interface Options {
+    view: string
+    actual: string
+    search: string
+    sort: {
+        sortBy: string
+        category: string
+        plan: string
+    }
+}
+
+const initialOptions = {
+    view: 'flex',
+    actual: 'today',
+    search: '',
+    sort: {
+        sortBy: 'A-Z',
+        category: 'all',
+        plan: 'pro'
+    }
+}
 
 const Deals = () => {
     const dispatch = useAppDispatch()
@@ -13,7 +37,8 @@ const Deals = () => {
     const {currentOption} = useAppSelector(state => state.sidebarSlice)
     const {theme} = useContext(ThemeContext) as ThemeContextType
     //make redux later
-    const [view, setView] = useState('flex')
+    const [options, setOptions] = useState<Options>(initialOptions)
+
 
     useEffect(() => {
         if (currentOption === 'My Deals') {
@@ -25,25 +50,76 @@ const Deals = () => {
         }
     }, [dispatch])
 
+
     return (
         <div className={cls.deals}>
-            <div className={cls.options}>
-                <h2>Вид</h2>
-                <button onClick={() => setView(view === 'grid' ? 'flex' : 'grid')}>
-                    {view}
-                </button>
+            <div className={`${cls.options} ${cls[theme]}`}>
+                <div className={cls.view}>
+                    <button onClick={() => setOptions(prev => ({...prev, view: prev.view === 'grid' ? 'flex' : 'grid'}))}>
+                        <img
+                            src={require(`../../../../assets/images/svg/${theme}/${options.view === 'flex' ? 'grid' : 'row'}.svg`)}
+                            alt="View"
+                        />
+                    </button>
+                </div>
+
+                <div className={cls.actual}>
+                    <div
+                        onClick={() => setOptions(prev => ({...prev, actual: 'today'}))}
+                        className={`${cls.option} ${options.actual === 'today' ? cls.active : ''}`}
+                    >
+                        <p>Today <span>5</span></p>
+                    </div>
+                    <div
+                        onClick={() => setOptions(prev => ({...prev, actual: 'all'}))}
+                        className={`${cls.option} ${options.actual === 'all' ? cls.active : ''}`}
+                    >
+                        <p>All <span>999</span></p>
+                    </div>
+                </div>
+                <div className={cls.search}>
+                    <input
+                        type="text"
+                        placeholder='Search'
+                        onChange={e => setOptions(prev => ({...prev, search: e.target.value}))}
+                        value={options.search}
+                    />
+                </div>
+                <div className={cls.sort}>
+                    <select onChange={e => setOptions(prev => ({...prev, sort: {...prev.sort, category: e.target.value}}))}>
+                        <option
+                            value='all'
+                        >
+                            {options.sort.category === 'all' ? 'All categories' : capitalize(options.sort.category)}
+                        </option>
+                    </select>
+                    <select onChange={e => setOptions(prev => ({...prev, sort: {...prev.sort, category: e.target.value}}))}>
+                    <option value='all'>
+                            Sort by {options.sort.sortBy}
+                        </option>
+                    </select>
+                    <select onChange={e => setOptions(prev => ({...prev, sort: {...prev.sort, category: e.target.value}}))}>
+                    <option value='all'>
+                            {capitalize(options.sort.plan)}
+                        </option>
+                    </select>
+                </div>
             </div>
-            <div className={`${cls.items} ${cls[view]}`}>
+            <div className={`${cls.items} ${cls[options.view]}`}>
                 {
                     isLoading
-                        ? <img src={require('../../../../assets/images/svg/loading.svg').default} alt="Loading..."/>
+                        ? <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                            <img src={require('../../../../assets/images/svg/loading.svg').default} alt="Loading..."/>
+                        </div>
                         // : deals.map(deal => {
-                        : [1,1,1,1,1,1,1,1,1].map(() => {
+                        : options.actual === 'today'
+                        ? [1,1,1,1,1,1,1,1,1].map(() => {
                             console.log(deal)
                             return <>
                                 <DealCard item={deal} theme={theme}/>
                             </>
                         })
+                            : <h1>ALL DEALS (Придумать дизайн)</h1>
                 }
             </div>
         </div>
