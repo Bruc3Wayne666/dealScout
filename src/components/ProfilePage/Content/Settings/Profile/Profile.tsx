b4eb75dadde6b46b01a9545410aa44156430226e
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2023-07-07 15:53:11 +0300

    some changes

--- src/components/ProfilePage/Content/Settings/Profile/Profile.tsx
+++ src/components/ProfilePage/Content/Settings/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC, useEffect, useRef, useState} from 'react';
 import cls from './Profile.module.scss'
 import {useTranslation} from "react-i18next";
 
@@ -7,19 +7,48 @@ interface ProfileProps {
 }
 
 const Profile: FC<ProfileProps> = ({theme}) => {
+    const [login, setLogin] = useState('')
+    const [img, setImg] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        // @ts-ignore
+        const file = e.currentTarget.files[0]
+        setImg(URL.createObjectURL(file))
+    }
+
     const {t} = useTranslation('profile')
     return (
-        <div className={cls.profile}>
+        <div className={`${cls.profile} ${cls[theme]}`}>
             <div className={cls.change}>
-                <h4>{t('change_image')}</h4>
-                <input type="file"/>
+                <div className={cls.image}>
+                    {/*<h4>{t('change_image')}</h4>*/}
+                    <div className={cls.input}>
+                        <img
+                            src={img || require(`../../../../../assets/images/svg/${theme}/profile.svg`)}
+                            alt="Profile Image"
+                        />
+                        <input
+                            accept='image/*'
+                            onChange={handleChange}
+                            type="file"
+                        />
+                    </div>
+                </div>
 
-                <h4>{t('change_login')}</h4>
-                <input
-                    className={cls[theme]}
-                    placeholder={t('type_login')}
-                    type="text"
-                />
+                <div className={cls.login}>
+                    <h4>{t('change_login')}</h4>
+                    <input
+                        className={cls[theme]}
+                        placeholder={t('type_login')}
+                        type="text"
+                        value={login}
+                        onChange={e => setLogin(e.currentTarget.value)}
+                    />
+                    {
+                        login &&
+                        <button>{t('confirm')}</button>
+                    }
+                </div>
             </div>
         </div>
     );
