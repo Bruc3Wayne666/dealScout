11a24814c723ef23451b5bdfc4b91909dee53a04
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2023-07-14 18:35:26 +0300

    -_-

--- src/components/ProfilePage/Content/Dashboard/Header/Header.tsx
+++ src/components/ProfilePage/Content/Dashboard/Header/Header.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import cls from './Header.module.scss';
+import {ThemeContext, ThemeContextType} from "../../../../../providers/ThemeProvider";
 
 const Header = () => {
+    const {theme} = useContext(ThemeContext) as ThemeContextType
     return (
         <div className={cls.header}>
-            header
+            <h1>Statistics</h1>
+            <div className={`${cls.buttons} ${cls[theme]}`}>
+                <button>[PLAN]</button>
+                <button/>
+            </div>
         </div>
     );
 };
