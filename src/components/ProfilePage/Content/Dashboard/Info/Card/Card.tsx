d0a8f6538f4860e01d3a1a21f85e8c79fed2f1af
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2023-07-14 20:02:27 +0300

    -_-

--- src/components/ProfilePage/Content/Dashboard/Info/Card/Card.tsx
+++ src/components/ProfilePage/Content/Dashboard/Info/Card/Card.tsx
@@ -1,13 +1,17 @@
-import React, {FC} from 'react';
+import React, {FC, forwardRef} from 'react';
 import cls from './Card.module.scss';
+import {motion} from "framer-motion";
 
 
-const Card: FC<any> = ({children}) => {
+const Card: FC<any> = forwardRef(({children}, ref: any) => {
     return (
-        <div className={cls.card}>
+        <div
+            ref={ref}
+            className={cls.card}
+        >
             {children}
         </div>
     );
-};
+})
 
-export default Card;
+export default motion(Card);
--- src/components/ProfilePage/Content/Dashboard/Info/Info.tsx
+++ src/components/ProfilePage/Content/Dashboard/Info/Info.tsx
@@ -3,20 +3,57 @@ import cls from './Info.module.scss';
 import Card from "./Card/Card";
 import ChartContent from "./Card/Chart/Chart";
 import Report from "./Card/Report/Report";
+import {dashAnimation} from "../../../../../global/animations/profilePage";
 
 const Info = () => {
     return (
         <div className={cls.info}>
-            <Card>
+            <Card
+                custom={1}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            >
                 <ChartContent/>
             </Card>
-            <Card>
+            <Card
+                custom={2}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            >
                 <Report/>
             </Card>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card
+                custom={3}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            />
+            <Card
+                custom={4}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            />
+            <Card
+                custom={5}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            />
+            <Card
+                custom={6}
+                initial={'hidden'}
+                whileInView={'visible'}
+                variants={dashAnimation}
+                viewport={{once: true}}
+            />
         </div>
     );
 };
